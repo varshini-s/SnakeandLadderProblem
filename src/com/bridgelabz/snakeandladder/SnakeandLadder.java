edee22b6918232c52d2abaa4a01d5d2f48328b9d
varshini <dreamvarshini@gmail.com>
2021-08-20 18:47:28 +0530

    MODIFIED:new use case UC1-start

--- src/com/bridgelabz/snakeandladder/SnakeandLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeandLadder.java
@@ -1,12 +1,20 @@
 package com.bridgelabz.snakeandladder;
 
 public class SnakeandLadder {
+	public static final int maxVal=7;
+	public static final int minVal=1;
+	
 	
 	public static void main(String args[]) {
 		
 		System.out.println("------------Welcome to SnakeandLadder Problem------------");
 		
-		int currPos=0;
+		int startPos=0;
+		System.out.println("Starting postion is:"+startPos);
+		
+		
+		
+		
 		
 	}
 	
