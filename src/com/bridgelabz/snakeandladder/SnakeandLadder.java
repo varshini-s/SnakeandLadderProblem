6e6e05e4a433cb5db725b68500ccd6a921e5c0d6
varshini <dreamvarshini@gmail.com>
2021-08-20 18:01:39 +0530

    ADDED:new file in master branch

---
+++ src/com/bridgelabz/snakeandladder/SnakeandLadder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeandLadder {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("------------Welcome to SnakeandLadder Problem------------");
+		
+	}
+	
+
+}
